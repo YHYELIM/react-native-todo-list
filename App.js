a7cbb85912ec5751d2dcd2a837e2dc5fd777f905
chaechaebing <chaechaebing@gmail.com>
2024-07-17 18:19:33 +0900

    FlatList 렌더링

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 import DateHead from './components/DateHeader';
 import AddTodo from './components/AddTodo';
 import Empty from './components/\bEmpty';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import TodoList from './components/\bTodoList';
 
 function App() {
   const today = new Date();
-  console.log(today);
+  // todos 항목 선언
+  // id : 각 항목 고유 id 값
+  // text : 화면에서 보여줄 text 값
+  // done : 할일의 완료 여부 값
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.block} edges={['bottom']}>
         <View style={styles.container}>
           <DateHead date={today} />
-          <Empty />
+          {todos.length === 0 ? <Empty /> : <TodoList todoing={todos} />}
           <AddTodo />
         </View>
       </SafeAreaView>
@@ -30,7 +40,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
- 
 });
 
 export default App;
---
+++ components/TodoList.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {FlatList, View, Text, StyleSheet} from 'react-native';
+import TodoItem from './\bTodoItem';
+function TodoList({todoing}) {
+  //console.log('222', todoing);
+  return (
+    <FlatList
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+      style={styles.list}
+      // FlatList에 렌더링할 todos 배열 전달
+      data={todoing}
+      // 여기 데이터 가지고
+      // 아이템 렌더링 방법 정의
+      // item : todos 배열의 각 요소
+      // 각 아이템의 text 화면에 표시
+      renderItem={({item}) => (
+        <TodoItem id={item.id} text={item.text} done={item.done} />
+      )}
+      keyExtractor={item => item.id.toString()}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+  },
+  separator: {
+    backgroundColor: '#e0e0e0',
+    height: 1,
+  },
+});
+export default TodoList;
