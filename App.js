51500a0e3e41611d064ed61c403cd42f5dee6c64
chaechaebing <chaechaebing@gmail.com>
2024-07-17 11:00:17 +0900

    이미지 넣기 완료

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 import DateHead from './components/DateHeader';
+import AddTodo from './components/AddTodo';
+import Empty from './components/\bEmpty';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App() {
   const today = new Date();
   console.log(today);
   return (
-    <SafeAreaView>
-      <View>
-        <DateHead date={today} />
-        <Text>TodoApp</Text>
-      </View>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.block} edges={['bottom']}>
+        <View style={styles.container}>
+          <DateHead date={today} />
+          <Empty />
+          <AddTodo />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+ 
+});
 
 export default App;
--- components/DateHeader.js
+++ components/DateHeader.js
@@ -5,7 +5,7 @@ function DateHead({date}) {
   // 받은 데이터값 추출
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
-  const day = date.getDay();
+  const day = date.getDate(); // getDate : 날짜 반환 , getDay : 요일 반환
   const formatted = `${year}년 ${month}월 ${day}일`;
 
   return (
