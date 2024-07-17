72384b671b4a846e2190a63f826c122a0025c829
chaechaebing <chaechaebing@gmail.com>
2024-07-17 12:52:51 +0900

    버튼 추가

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Image, TouchableOpacity} from 'react-native';
 
 function AddTodo() {
   const [text, setText] = useState('');
@@ -15,6 +15,12 @@ function AddTodo() {
         // 호출될 때는 현재 TextInput 내용을 인자로 넣어서 호출
         onChangeText={setText}
       />
+      {/* 버튼 눌렸을 때 효과 보여줌 */}
+      <TouchableOpacity activeOpacity={0.5}>
+      <View style={styles.buttonStyle}>
+        <Image source={require('../assets/icons/add_white/add_white.png')} />
+      </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -24,11 +30,22 @@ const styles = StyleSheet.create({
     borderColor: '#bdbdbd',
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   input: {
+    flex: 1,
     fontSize: 16,
     paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    borderRadius: 24,
+    marginLeft: 16, // TextInput과 버튼 사이에 간격 추가
   },
 });
 export default AddTodo;
