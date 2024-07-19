03e72d2505b01efd667e5c19aee1d12356ce94d3
chaechaebing <chaechaebing@gmail.com>
2024-07-19 17:16:17 +0900

    useEffect + AsyncStorage 사용

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 import DateHead from './components/DateHeader';
 import AddTodo from './components/AddTodo';
 import Empty from './components/\bEmpty';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import TodoList from './components/\bTodoList';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import todoStorage from './storages/todoStorage';
 
 function App() {
   const today = new Date();
@@ -17,6 +19,33 @@ function App() {
     {id: 2, text: '리액트 네이티브 기초 공부', done: false},
     {id: 3, text: '투두리스트 만들어보기', done: false},
   ]);
+
+  // useEffect 이용한 todos 불러오기
+  // 처음 렌더링 될 때 실행
+
+  // saveStorage 추상화 시킴
+  // 데이터를 불러오고 나서 그 결과물을 setTodos의 함수 인자로 넣어 호출
+  useEffect(() => {
+    todoStorage.get().then(setTodos).catch(console.error);
+  }, []);
+
+  // useEffect
+  // 특정 상태가 바뀔 때마다 특정 함수 호출
+  // [todos] : 의존성 배열, todos라는 상태 구독하는 중임을 알수있음
+  // 첫 번째 인자(함수): 구독하고 있는 값이 바뀌었을 때 호출하고 싶은 함수 넣기
+  // 두 번째 인자(의존성 배열) : 구독하고 있는 값을 배열 안에 넣음
+  // [todos] : todos 변경될 때마다 useEffect 첫번째 인자로 전달한 함수 실행
+  // useEffect 등록 함수 : 두 번째 인자의 배열 안에 넣은 값이 호출 , 컴포넌트 최초 렌더링 될 때 호출
+
+  //useEffect 이용한 저장
+  // useEffect함수 내부에서 AsyncStorage - setItem메서드 사용
+  // async 내부에 선언 : useEffect
+
+  // saveStorage 추상화 시킴
+  useEffect(() => {
+    todoStorage.set(todos).catch(console.error);
+  }, [todos]);
+
   const onInsert = text => {
     const nextId =
       // 새로 등록할 항목의 id 구함
---
+++ storages/todoStorage.js
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// 추가로 데이터 불러오거나 저장할 때 사용하는 key 값 따로 상단에 상수로 선언
+// 추후 손쉽게 바꾸기 가능
+const key = 'todos';
+
+// todoStorage 객체 만듬
+// 겍체 내부에 get , set 메서드 만듬
+
+const todoStorage = {
+  async get() {
+    try {
+      const rawTodos = await AsyncStorage.getItem(key);
+
+      if (!rawTodos) {
+        //저장된 데이터가 없으면 사용하지 않음
+        throw new Error('No saved todos');
+      }
+      const savedTodos = JSON.parse(rawTodos);
+      return savedTodos;
+    } catch (e) {
+      throw new Error('Failed to load todos');
+    }
+  },
+  async set() {
+    try {
+      // eslint-disable-next-line no-undef
+      await AsyncStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      throw new Error('Failed to load todos');
+    }
+  },
+};
+export default todoStorage;
